d405c1e784870f0cbdd365d28bf405d5c98b9ae5
Alexander <alexandertrudolyubov@gmail.com>
2022-03-08 17:40:27 +0200

    Create Pygame.py

---
+++ PythonGroup/Module 4/Lesson 1.0/Alexander/Pygame.py
@@ -0,0 +1,30 @@
+class Player():
+    weapon = ""
+    bullets = 0
+    bulletType = ""
+
+def Amory(self):
+        weaon.hold = Shotgun
+        bullets.num = 999
+        bulletType = HeavyBullet
+        Amory = Something
+        print(Something)
+
+
+class Database():
+    Player = ""
+    PlayerHash = 0
+    Tier = 0
+    def Secretinfo(self):
+        player.nick = "Jovan"
+        PlayerHash.tag = 1663
+        Secretinfo = "Shh!"
+
+    def Level(self):
+        Tier.lvl = 37   
+        Tier.total = 100   
+
+    If Player Tier.lvl = 30 <
+        print ("Noob")
+    else:
+        print("PRO")
