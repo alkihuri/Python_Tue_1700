b65b1ec597995b4899ad7470d18858f2e872a48a
RokasCy <101199579+RokasCy@users.noreply.github.com>
2022-04-12 17:14:34 +0300

    Update Game.py

--- PythonGroup/Module 4/Lesson 5.0 Shooter 1.0/Game.py
+++ PythonGroup/Module 4/Lesson 5.0 Shooter 1.0/Game.py
@@ -1,101 +1,39 @@
-print("Robolab Python Pro Course / Shooter template project =) ")
+import pygame
+from pygame import *
+import keyboard
+from os import *
 
+window = display.set_mode((400, 640))
+window = display.set_caption('ShooterGame')
 
-# link for presentation => https://docs.google.com/presentation/d/1UkxfjWFpecW7BD-9ngFL7hs1fSykJnfQynp53x15_i8/edit#slide=id.g122dbdc220d_1_85
+img_dir = path.join(path.dirname(__file__), 'img') 
+spaceShip_img = pygame.image.load(path.join(img_dir, "ship.png")).convert() 
 
+class Player(pygame.sprite.Sprite):
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.transform.scale(spaceShip_img, (50, 38))
+        self.rect = self.image.get_rect()
+        self.speedx = 0
+    def update(self):
+        if keyboard.is_pressed('a'):
+            print('a')
+            speedx += -8
+        if keyboard.is_pressed('d'):
+            print('d')
+            speedx += 8
 
-# modules import 
+        self.rect.x += self.speedx 
 
-
-
-
-
-
-
-# colors set up
-
-
-
-
-
-
-
-
-# Window creating
-
-
-
-
- 
-# Game sprites 
-
-
-
-#player class
+player = Player()
  
 
+running = True
+while running:
 
+    for eachEvents in event.get():
+        if eachEvents.type == QUIT:
+            running = False
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- # mob class
-
-
-
-
-
-
-
-
-
-
-
-
-#bullet class
-
-
-
-
-#Game entities innit
-
-
-
-
-
-
-
-
-
-# Game Lyfecycle
-
-
-
-
-    # rendering
-
-
- 
-
-    #win lose  situation 
-    
-
-
-
- 
-    #text 
-     
 
  
