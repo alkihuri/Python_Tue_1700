0e9ac239d268596ab02e873780c1abd0f77af3ef
alkihuri <alkihuri@gmail.com>
2022-05-03 16:54:56 +0300

    Arcade 1.0 template

--- PythonGroup/Module 4/Lesson 6.0 Shooter 2.0/Game.py
+++ PythonGroup/Module 4/Lesson 6.0 Shooter 2.0/Game.py
@@ -23,11 +23,11 @@ pygame.display.set_caption("Shooter 2nd lesson")
 clock = pygame.time.Clock() 
 # Game sprites 
 img_dir = path.join(path.dirname(__file__),"img")
-background = pygame.image.load(path.join(img_dir,"field.png")).convert()
+background = pygame.load(path.join(img_dir,"field.png")).convert()
 background_rect = background.get_rect() 
-player_img = pygame.image.load(path.join(img_dir,"ship.png")).convert()
-npc_img = pygame.image.load(path.join(img_dir,"npc.png")).convert()
-bullet_img = pygame.image.load(path.join(img_dir,"bullet.png")).convert()
+player_img = pygame.load(path.join(img_dir,"field.png")).convert()
+npc_img = pygame.load(path.join(img_dir,"npc.png")).convert()
+bullet_img = pygame.load(path.join(img_dir,"bullet.png")).convert()
 
 #player class
 class Player(pygame.sprite.Sprite):
@@ -38,7 +38,7 @@ class Player(pygame.sprite.Sprite):
         self.image.set_colorkey(WHITE)
         self.rect = self.image.get_rect()
         self.rect.centerx = WIDTH /2
-        self.rect.bottom = HEIGHT - 50
+        self.rect.bottom = HEIGHT - player_size.x
         self.speedx = 0
 
     def update(self):
