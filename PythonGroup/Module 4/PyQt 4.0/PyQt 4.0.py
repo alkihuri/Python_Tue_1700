64e23d7e9f90bd9fd78e27d5150c2b387bf2bc07
alkihuri <alkihuri@gmail.com>
2022-06-14 18:11:21 +0300

    Update PyQt 4.0.py

--- PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
+++ PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
@@ -2,67 +2,71 @@
 #work with files READ and WRITE
 #presentation link https://docs.google.com/presentation/d/1bdxF5J2j9vmLCPbtVkgYI_0DsI1r05Yv_gwBHwv0ypk/edit?usp=sharing
 
-
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow,QComboBox 
 import sys
 import os 
 
-class ReaderView(QMainWindow):   
-    def __init__(self,myViewModel):
-        super(ReaderView,self).__init__()    
-        self.myViewModel = myViewModel                      
+class ReaderView(QMainWindow):  
+
+    def __init__(self,myViewModel): 
+        super(ReaderView,self).__init__()     
+        self.myViewModel = myViewModel                       
         self.setGeometry(0,0,600,600)                                        
-        self.setWindowTitle("Poem reader")
-        self.poemSelectField = self.CreateComboBox("Poem:",    150,25)    
+        self.setWindowTitle("Poem reader") 
+        self.poemSelectField = self.CreateComboBox("Poem:",    150,25)     
         self.poemSelectField.addItems(["WILLIAM SHAKESPEARE","RUDYARD KIPLING"])
-        self.poemField = self.CreateLabel("POEM NOT LOADED",150,100)  
-   
+        self.poemField = self.CreateLabel("POEM NOT LOADED",150,100) 
+
     def RequestPoem(self):
-        self.poemString = self.myViewModel.SetPath(self.poemSelectField.currentText())  
-        self.UpdateUI() 
-   
+        self.poemString = self.myViewModel.SetPath(self.poemSelectField.currentText())   
+        self.UpdateUI()
+    
     def CreateLabel(self,text,x,y):
         newLabel = QtWidgets.QLabel(self)
         newLabel.setText(text)
         newLabel.move(x,y)
-        return newLabel 
-    def CreateComboBox(self,label,x,y):
+        return newLabel
+
+    def CreateComboBox(self,label,x,y): 
         self.CreateLabel(label,x,y)
-        combo = QComboBox(self)
+        combo = QComboBox(self) 
         combo.move(x+50, y)
         combo.resize(200, 32)
         combo.activated[str].connect(self.RequestPoem)      
         return combo 
-   
-    def UpdateUI(self):
-        self.UpdateUI
-        lineOffset = 25
-        for x in self.poemString:
+
+    def UpdateUI(self): 
+        total = ""
+        lineOffset = 25 
+        for x in self.poemString: 
             total += x  + "\n"  
         self.poemField.resize(500,lineOffset * len(self.poemString))
-        self.poemField.Update
+        self.poemField.setText(total)
 
-class ReaderModelView():
+class ReaderModelView(): 
 
     def SetPath(self,path):
         currentFilePath     = os.path.dirname(__file__)                  
         poemFilePath = currentFilePath + "/" + path + ".txt"
-        connect(self.GetPoemFromModel(poemFilePath))
+        return self.GetPoemFromModel(poemFilePath)
 
     def GetPoemFromModel(self,path):
-        self.poem=ReaderModel.GetPoem(path)
+        return ReaderModel.GetPoem(path)
 
 class ReaderModel():  
     def GetPoem(path):
-        poem = list()
-        poem.move(x+50, y)
-        poem.resize(200, 32)
+        poem = list() 
+        f = open(path, "r")
+        for x in f:
             poem.append(x)
         return poem
 
-app = QApplication(sys.argv)   
-myViewModel = QApplication()
-myView = ReaderView(myViewModel)
+
+
+app = QApplication(sys.argv)  
+myModel = ReaderModel()
+myViewModel = ReaderModelView()
+myView = ReaderView(myViewModel) 
 myView.show()
 sys.exit(app.exec_())
