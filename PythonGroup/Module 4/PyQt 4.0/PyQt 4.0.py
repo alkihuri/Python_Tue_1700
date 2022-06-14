ba08c7b9e0bc0693d41550462d7411c309b14931
Nikdino2000 <101200586+Nikdino2000@users.noreply.github.com>
2022-06-14 18:06:49 +0300

    Update PyQt 4.0.py

--- PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
+++ PythonGroup/Module 4/PyQt 4.0/PyQt 4.0.py
@@ -7,3 +7,26 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication,QMainWindow,QComboBox 
 import sys
 import os 
+
+class ReaderView(QMainWindow):
+    def  init  (self,myViewModel):
+        super(ReaderView,self).__init__()
+        self.myViewModel = myViewModel
+        self.setGeometry(0,0,600,600)
+        self.setWindowTitle("Poem reader")
+        self.poemSelectField = self.CreateComboBox("Poem:",   150,25)
+        self.poemSelectField.addItems(["WILLIAM SHAKESPEAR","RUDYARD KIPLING"])
+        sdlf.poemField = self.CreateLabel("POEM NOT LOADED",150,100)
+
+def RequestPoem(self):
+    self.poemString = self.myViewModel.SetPath(self.poemSelectField.currentText())
+    self.UpdateUI()
+
+def CreateLabel(self,text,x,y):
+    newLabel = QtWidgets.QLabel(self)
+    newLabel.setText(text)
+    newLabel.move(x,y)
+    return newLabel
+
+def 
+
